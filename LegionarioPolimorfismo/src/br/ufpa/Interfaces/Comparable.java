4f5e59e4d18b9b4685fea00414b0ce4b564f6740
Ramon <ramonestii@hotmail.com>
2014-11-26 12:28:13 -0300

    Atualizacao
    Atualizacao interface comparable

--- LegionarioPolimorfismo/src/br/ufpa/Interfaces/Comparable.java
+++ LegionarioPolimorfismo/src/br/ufpa/Interfaces/Comparable.java
@@ -3,5 +3,12 @@ package br.ufpa.Interfaces;
 
 
 public interface Comparable<Arqueiro> {
+
+    /**
+     *
+     * @param um
+     * @param outro
+     * @return
+     */
     int compareTo(Arqueiro um, Arqueiro outro);
 }
