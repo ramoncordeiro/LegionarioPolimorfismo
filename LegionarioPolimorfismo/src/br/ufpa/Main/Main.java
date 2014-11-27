8e269ae958aa9bd12c3d8d09d743858ea4b66906
ramonestii <ramonestii@hotmail.com>
2014-11-27 00:13:57 -0300

    Atualizacao
    Atualizacao

--- LegionarioPolimorfismo/src/br/ufpa/Main/Main.java
+++ LegionarioPolimorfismo/src/br/ufpa/Main/Main.java
@@ -6,9 +6,19 @@ import br.ufpa.Personagens.Legionario;
 import br.ufpa.classesRelacionadas.Item;
 import br.ufpa.classesRelacionadas.Esquadrao;
 import br.ufpa.classesRelacionadas.Menu;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        Menu m = new Menu();
+      
+        JOptionPane.showMessageDialog(null,"Por favor Insira um valor para entrar no MENU");
+        JOptionPane.showMessageDialog(null,"1 - Criar Legionario,2- Criar Arqueiro, 3 - Criar Lanceiro");
+        
+      
+        Menu m;
+        m = new Menu();
+        
+        
+                
     }
 }
